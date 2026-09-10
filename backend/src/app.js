298e404a50b849fff39cfb5655d3820d2d6def70
yarnbucket <yarngm@gmail.com>
2026-09-09 18:05:19 -0700

    Mount Boxing resolver

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,6 +13,7 @@ import tennisRoute from "./routes/tennis.js";
 import soccerRoute from "./routes/soccer.js";
 import collegeSoccerRoute from "./routes/collegeSoccer.js";
 import mmaRoute from "./routes/mma.js";
+import boxingRoute from "./routes/boxing.js";
 import resolveRoute from "./routes/resolve.js";
 
 const app = express();
@@ -70,6 +71,7 @@ app.use("/resolve", tennisRoute);
 app.use("/resolve", soccerRoute);
 app.use("/resolve", collegeSoccerRoute);
 app.use("/resolve", mmaRoute);
+app.use("/resolve", boxingRoute);
 app.use("/resolve", resolveRoute);
 
 // Root endpoint
